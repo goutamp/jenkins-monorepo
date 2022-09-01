00d88a533c15ba25395bd7d12e12df0b15c0386e
Goutam Pramanick <pramanick.goutam@gmail.com>
2022-09-01 16:32:54 +0530

    Update HelloWorld.java

--- services/foo-service/HelloWorld.java
+++ services/foo-service/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World, again!"); 
+        System.out.println("This is from foo service!"); 
     }
 }
