2745a6de9a5f03c71c00556720f64ae604fbde26
gpramanick <gpramanick@beyondtrust.com>
2022-09-16 16:02:08 +0530

    trigger foo-service

--- services/foo-service/HelloWorld.java
+++ services/foo-service/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is from foo service testing!"); 
+        System.out.println("This is from foo service test!"); 
     }
 }
