05ceba38b8abef6d19f272d1b1bfcae60326ff58
gpramanick <gpramanick@beyondtrust.com>
2022-09-16 17:37:20 +0530

    change foo lib

--- lib/foo-lib/HelloWorld.java
+++ lib/foo-lib/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is from foo service test!"); 
+        System.out.println("This is from foo  lib test!"); 
     }
 }
