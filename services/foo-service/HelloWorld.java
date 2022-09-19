588fe05763575c3560f21e2d4beb98129374dc1a
gpramanick <gpramanick@beyondtrust.com>
2022-09-19 17:29:02 +0530

    change service

--- services/foo-service/HelloWorld.java
+++ services/foo-service/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is from foo service1!"); 
+        System.out.println("This is from foo service!"); 
     }
 }
