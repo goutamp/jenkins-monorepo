493a966ed68b21af296d597c8ad7bf7248c68a1f
gpramanick <gpramanick@beyondtrust.com>
2022-09-19 18:16:11 +0530

    stopping lib pipeline

--- services/foo-service/HelloWorld.java
+++ services/foo-service/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is from foo service update!"); 
+        System.out.println("This is from foo service update  test!"); 
     }
 }
