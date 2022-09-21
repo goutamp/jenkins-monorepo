72d30e4f9babd071a3bee75de9d95641621f5fe1
gpramanick <gpramanick@beyondtrust.com>
2022-09-21 19:02:37 +0530

    test

--- services/foo-service/HelloWorld.java
+++ services/foo-service/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is from changes in the servicetest!"); 
+        System.out.println("This is from change in the servicetest!"); 
     }
 }
